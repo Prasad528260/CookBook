92c21ec1c3276d6d97de04a7312ae3a6af0877d8
Prasad528260 <prasadsubhedar5460@gmail.com>
2024-09-16 18:25:53 +0530

    commit by prasad
    now i have completed the image map.now only about us section remaining

--- My Project/JavaScript/map3.js
+++ My Project/JavaScript/map3.js
@@ -1,8 +1,16 @@
 var recipes = {
-  Delhi: "https://example.com/recipes/delhi",
-  Mumbai: "https://example.com/recipes/mumbai",
-  Chennai: "https://example.com/recipes/chennai",
-  Kolkata: "https://example.com/recipes/kolkata",
+  Delhi: "#",
+  Mumbai: "#",
+  Chennai: "#",
+  Kolkata: "#",
+};
+
+var coordinates = {
+  Delhi: { top: '35%', left: '35%' },
+  Mumbai: { top: '65%', left: '25%' },
+  Chennai: { top: '79%', left: '45%' },
+  Kolkata: { top: '50%', left: '65%' },
+
 };
 
 $(".search-button").click(function () {
@@ -10,21 +18,39 @@ $(".search-button").click(function () {
 
   if (location) {
     var recipeUrl = recipes[location];
+    var position = coordinates[location];
 
     if (recipeUrl) {
       var cardHtml = `
-                    <div class="card">
-                        <h3>Recipe for ${location}</h3>
-                        <a href="${recipeUrl}" target="_blank">View Recipe</a>
-                    </div>
-                `;
+        <div class="card">
+            <h3>Popular recipe at ${location}</h3>
+            <a href="${recipeUrl}" target="_blank">View Recipe</a>
+        </div>
+      `;
       $("#card-container").html(cardHtml);
+
+      if (position) {
+        
+        $("#map").attr("src", "../Images/india.jpg"); 
+        $("#pin").css({
+          display: 'block',
+          top: position.top,
+          left: position.left
+        });
+      } else {
+        $("#map").attr("src", "../Images/india.jpg");
+        $("#pin").hide();
+      }
     } else {
       $("#card-container").html(
         '<div class="card"><p>No recipes found for this location.</p></div>'
       );
+      $("#map").attr("src", "../Images/india.jpg");
+      $("#pin").hide();
     }
   } else {
     $("#card-container").html("");
+    $("#map").attr("src", "../Images/india.jpg"); 
+    $("#pin").hide();
   }
 });
