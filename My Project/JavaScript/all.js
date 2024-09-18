7d52c3ddb3e7badbd11d0038e1aabd37ed5c2dc7
Prasad528260 <prasadsubhedar5460@gmail.com>
2024-09-18 21:30:33 +0530

    bookmark added by prasad

--- My Project/JavaScript/all.js
+++ My Project/JavaScript/all.js
@@ -3,3 +3,10 @@
         $(".tags-list a").removeClass("now");
         $(this).addClass("now");
     });
+    const bookmarkIcon = document.getElementById('bookmarkIcon');
+
+bookmarkIcon.addEventListener('click', function () {
+    this.classList.toggle('fas'); 
+    this.classList.toggle('far'); 
+    this.classList.toggle('active'); 
+});
